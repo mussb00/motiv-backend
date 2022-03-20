bbc684fc0ae3f473c034efd064bb5028abca13fa
mussb00 <berhanem2000@gmail.com>
2022-03-20 15:35:38 +0000

    added token verification

--- api/controllers/eventRoutes.js
+++ api/controllers/eventRoutes.js
@@ -1,7 +1,8 @@
 const express = require('express')
+const { verifyToken } = require('../middleware/auth')
 const router = express.Router()
 const greeting = require('../models/event')
 
-router.get('/greeting', greeting)
+router.get('/greeting', verifyToken, greeting)
 
 module.exports = router
